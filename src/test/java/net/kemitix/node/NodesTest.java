51e8194db76037a10619f3c493b1064ea1bd75f7
Paul Campbell <pcampbell@kemitix.net>
2016-08-21 11:37:15 +0100

    Nodes: add utility class to create Node instances

---
+++ src/test/java/net/kemitix/node/NodesTest.java
@@ -0,0 +1,62 @@
+package net.kemitix.node;
+
+import lombok.val;
+import org.assertj.core.api.SoftAssertions;
+import org.junit.Test;
+
+import static net.trajano.commons.testing.UtilityClassTestUtil
+        .assertUtilityClassWellDefined;
+
+/**
+ * Tests for {@link Nodes}.
+ *
+ * @author pcampbell
+ */
+public class NodesTest {
+
+    @Test
+    public void shouldBeValidUtilityClass() throws Exception {
+        assertUtilityClassWellDefined(Nodes.class);
+    }
+
+    @Test
+    public void shouldCreateUnnamedRoot() throws Exception {
+        val node = Nodes.unnamedRoot("data");
+        SoftAssertions softly = new SoftAssertions();
+        softly.assertThat(node.getData()).contains("data");
+        softly.assertThat(node.getName()).isNull();
+        softly.assertAll();
+    }
+
+    @Test
+    public void shouldCreateNamedRoot() throws Exception {
+        val node = Nodes.namedRoot("data", "name");
+        SoftAssertions softly = new SoftAssertions();
+        softly.assertThat(node.getData()).contains("data");
+        softly.assertThat(node.getName()).isEqualTo("name");
+        softly.assertAll();
+    }
+
+    @Test
+    public void shouldCreateUnnamedChild() throws Exception {
+        val parent = Nodes.unnamedRoot("root");
+        val node = Nodes.unnamedChild("data", parent);
+        SoftAssertions softly = new SoftAssertions();
+        softly.assertThat(node.getData()).contains("data");
+        softly.assertThat(node.getName()).isNull();
+        softly.assertThat(node.getParent()).contains(parent);
+        softly.assertAll();
+    }
+
+    @Test
+    public void shouldCreateNamedChild() throws Exception {
+        val parent = Nodes.unnamedRoot("root");
+        val node = Nodes.namedChild("data", "child", parent);
+        SoftAssertions softly = new SoftAssertions();
+        softly.assertThat(node.getData()).contains("data");
+        softly.assertThat(node.getName()).isEqualTo("child");
+        softly.assertThat(node.getParent()).contains(parent);
+        softly.assertAll();
+    }
+
+}
