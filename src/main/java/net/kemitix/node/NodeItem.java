daf982188748d06458e4521a830e9847f2372007
Paul Campbell <pcampbell@kemitix.net>
2018-10-09 22:08:43 +0100

    Switch to trunk-based-development (#34)
    * pom.xml: version set to 0.8.0-SNAPSHOT
    
    * jenkins: added
    
    * mvn: add wrapper 3.5.3
    
    * Upgrade parent to 5.1.0
    
    * lombok: upgraded to 1.16.20
    
    * assertj: upgraded to 3.9.1
    
    * coveralls: removed
    
    * kemitix-maven-tiles: added
    
    * kemitix-checkstyle: tile added
    
    * Compile with java 1.8
    
    * Reduce code coverage requirements
    
    * LICENSE: update
    
    * checkstyle: suppress outstanding npath complexity issues, etc
    
    * Bump kemitix-parent from 5.1.0 to 5.1.1
    
    Bumps [kemitix-parent](https://github.com/kemitix/kemitix-parent) from 5.1.0 to 5.1.1.
    - [Release notes](https://github.com/kemitix/kemitix-parent/releases)
    - [Changelog](https://github.com/kemitix/kemitix-parent/blob/master/CHANGELOG)
    - [Commits](https://github.com/kemitix/kemitix-parent/compare/v5.1.0...v5.1.1)
    
    Signed-off-by: dependabot[bot] <support@dependabot.com>
    
    * Bump tiles-maven-plugin from 2.10 to 2.12
    
    Bumps [tiles-maven-plugin](https://github.com/repaint-io/maven-tiles) from 2.10 to 2.12.
    - [Release notes](https://github.com/repaint-io/maven-tiles/releases)
    - [Changelog](https://github.com/repaint-io/maven-tiles/blob/master/CHANGELOG.adoc)
    - [Commits](https://github.com/repaint-io/maven-tiles/compare/tiles-maven-plugin-2.10...tiles-maven-plugin-2.12)
    
    Signed-off-by: dependabot[bot] <support@dependabot.com>
    
    * Bump assertj-core from 3.9.1 to 3.11.0
    
    Bumps [assertj-core](https://github.com/joel-costigliola/assertj-core) from 3.9.1 to 3.11.0.
    - [Release notes](https://github.com/joel-costigliola/assertj-core/releases)
    - [Commits](https://github.com/joel-costigliola/assertj-core/compare/assertj-core-3.9.1...assertj-core-3.11.0)
    
    Signed-off-by: dependabot[bot] <support@dependabot.com>
    
    * Bump lombok from 1.16.20 to 1.18.2
    
    Bumps [lombok](https://github.com/rzwitserloot/lombok) from 1.16.20 to 1.18.2.
    - [Release notes](https://github.com/rzwitserloot/lombok/releases)
    - [Changelog](https://github.com/rzwitserloot/lombok/blob/master/doc/changelog.markdown)
    - [Commits](https://github.com/rzwitserloot/lombok/compare/v1.16.20...v1.18.2)
    
    Signed-off-by: dependabot[bot] <support@dependabot.com>
    
    * Bump assertj-core from 3.11.0 to 3.11.1
    
    Bumps [assertj-core](https://github.com/joel-costigliola/assertj-core) from 3.11.0 to 3.11.1.
    - [Release notes](https://github.com/joel-costigliola/assertj-core/releases)
    - [Commits](https://github.com/joel-costigliola/assertj-core/compare/assertj-core-3.11.0...assertj-core-3.11.1)
    
    Signed-off-by: dependabot[bot] <support@dependabot.com>
    
    * Bump kemitix-parent from 5.1.1 to 5.2.0
    
    Bumps [kemitix-parent](https://gitlab.com/kemitix/kemitix-parent) from 5.1.1 to 5.2.0.
    - [Release notes](https://gitlab.com/kemitix/kemitix-parent/tags)
    - [Commits](https://gitlab.com/kemitix/kemitix-parent/commits/master)
    
    Signed-off-by: dependabot[bot] <support@dependabot.com>
    
    * Update CHANGELOG
    
    * Update Jenkinsfile
    
    * Remove maven wrapper
    
    * Remove travis config
    
    * Simplify .gitignore
    
    * [readme] convert to org-mode format
    
    * lombok scope is provided
    
    * version set to DEV-SNAPSHOT
    
    * rename property kemitix-tiles as kemitix-maven-tiles
    
    * Bump kemitix-maven-tiles from 0.8.1 to 1.3.0
    
    * Bump kemitix-maven-tiles from 1.3.0 to 1.3.1
    
    * NoteItem remove @ToString
    
    * Narrow scope of forceParent to package-private
    
    * [coverage] set required instructions covered to 97%

--- src/main/java/net/kemitix/node/NodeItem.java
+++ src/main/java/net/kemitix/node/NodeItem.java
@@ -1,31 +1,27 @@
-/*
-The MIT License (MIT)
-
-Copyright (c) 2016 Paul Campbell
-
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-SOFTWARE.
-*/
+/**
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2018 Paul Campbell
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software
+ * and associated documentation files (the "Software"), to deal in the Software without restriction,
+ * including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so,
+ * subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies
+ * or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+ * INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE
+ * AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+ * DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
 
 package net.kemitix.node;
 
 import lombok.NonNull;
-import lombok.ToString;
 import lombok.val;
 
 import java.util.Arrays;
@@ -42,7 +38,6 @@ import java.util.stream.Stream;
  *
  * @author Paul Campbell (pcampbell@kemitix.net)
  */
-@ToString(exclude = {"children", "data", "parent"})
 @SuppressWarnings("methodcount")
 class NodeItem<T> implements Node<T> {
 
@@ -76,7 +71,7 @@ class NodeItem<T> implements Node<T> {
      *
      * @param newParent The new parent node
      */
-    protected void forceParent(final Node<T> newParent) {
+    void forceParent(final Node<T> newParent) {
         this.parent = newParent;
     }
 
@@ -141,6 +136,7 @@ class NodeItem<T> implements Node<T> {
         doSetParent(parent);
     }
 
+    @SuppressWarnings("npathcomplexity")
     private void doSetParent(@NonNull final Node<T> newParent) {
         if (this.equals(newParent) || newParent.isDescendantOf(this)) {
             throw new NodeException("Parent is a descendant");
@@ -264,6 +260,7 @@ class NodeItem<T> implements Node<T> {
      * @return the child or null
      */
     @Override
+    @SuppressWarnings("npathcomplexity")
     public Optional<Node<T>> findInPath(@NonNull final List<T> path) {
         if (path.isEmpty()) {
             return Optional.empty();
@@ -332,6 +329,7 @@ class NodeItem<T> implements Node<T> {
     }
 
     @Override
+    @SuppressWarnings("movevariableinsideif")
     public String drawTree(final int depth) {
         final StringBuilder sb = new StringBuilder();
         final String unnamed = "(unnamed)";
